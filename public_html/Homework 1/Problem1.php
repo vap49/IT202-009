c0bf366904b7796e345a2e01e13a8e07e677303f
vap49 <vap49@njit.edu>
2021-09-19 03:31:36 -0400

    solved homework

--- public_html/Homework 1/Problem1.php
+++ public_html/Homework 1/Problem1.php
@@ -8,6 +8,9 @@ function processArray($arr) {
     echo "<br>Processing Array:<br><pre>" . var_export($arr, true) . "</pre>";
     echo "<br>Odds output:<br>";
     //TODO add logic here to echo out only odd values
+    foreach ($arr as $num){
+        if ($num % 2 == 1) echo $num;
+    }
 }
 echo "Problem 1: Odd Output<br>";
 ?>
--- public_html/Homework 1/Problem3.php
+++ public_html/Homework 1/Problem3.php
@@ -8,7 +8,10 @@ function bePositive($arr) {
     echo "<br>Processing Array:<br><pre>" . var_export($arr, true) . "</pre>";
     echo "<br>Positive output:<br>";
     //TODO use echo to output all of the values as positive (even if they were originally positive)
-
+    foreach ($arr as $num){
+        if ($num < 0 ) echo $num * -1;
+        else echo $num;
+    }
 }
 echo "Problem 3: Be Positive<br>";
 ?>
