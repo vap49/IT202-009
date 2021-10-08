b02b1aff14cedfa9c420fe5cf868e100805ca44d
vap49 <vap49@njit.edu>
2021-10-07 21:05:11 -0400

    working

--- public_html/Project/home.php
+++ public_html/Project/home.php
@@ -1,13 +1,14 @@
 <?php
-require(__DIR__."/../../partials/nav.php");
+require(__DIR__ . "/../../partials/nav.php");
 ?>
 <h1>Home</h1>
 <?php
-if(is_logged_in()){
- flash("Welcome, " . get_user_email()); 
-}
-else{
-  flash("You're not logged in");
+if (is_logged_in()) {
+    echo "Welcome home, " . get_user_email();
+    //comment this out if you don't want to see the session variables
+    echo "<pre>" . var_export($_SESSION, true) . "</pre>";
+} else {
+    echo "You're not logged in";
 }
 ?>
-<?php require(__DIR__ . "/../../partials/flash.php");?>
+<?php require(__DIR__ . "/../../partials/flash.php"); ?>
