eb46709e08b09b142d0df28711bc5f0c8332209a
vap49 <vap49@njit.edu>
2021-10-28 13:06:40 -0400

    User Roles Feature

--- public_html/Project/register.php
+++ public_html/Project/register.php
@@ -77,6 +77,7 @@ if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm
         try {
             $stmt->execute([":email" => $email, ":password" => $hash]);
             flash("You've registered, yay...");
+            die(header("Location: login.php"));
         } catch (Exception $e) {
             flash("There was a problem registering");
             flash("<pre>" . var_export($e, true) . "</pre>");
