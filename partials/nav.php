537e48d952fde64fe84c27c4327e80f44e08474b
vap49 <vap49@njit.edu>
2021-11-24 20:26:49 -0500

    Purchase Items

--- partials/nav.php
+++ partials/nav.php
@@ -31,14 +31,14 @@ require_once(__DIR__ . "/../lib/functions.php");
 
 <nav class="navbar navbar-expand-lg navbar-light bg-info">
     <div class="container-fluid">
-        <a class="navbar-brand" href="<?php echo get_url('home.php'); ?>">Bamazon.com</a> 
+        <a class="navbar-brand" href="<?php echo get_url('shop.php'); ?>">Bamazon.com</a> 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navContent" aria-controls="navContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <?php if (is_logged_in()) : ?>
-                    <li class="nav-item"><a class="nav-link" href="<?php echo get_url('home.php'); ?>">Home</a></li>
+                   <!-- <li class="nav-item"><a class="nav-link" href="<?php #echo get_url('home.php'); ?>">Home</a></li> -->
                     <li class="nav-item"><a class="nav-link" href="<?php echo get_url('profile.php'); ?>">Profile</a></li>
                 <?php endif; ?>
                 <?php if (!is_logged_in()) : ?>
@@ -50,10 +50,20 @@ require_once(__DIR__ . "/../lib/functions.php");
                         <a class="nav-link dropdown-toggle" href="#" id="rolesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                             Admin Roles
                         </a>
-                        <ul class="dropdown-menu bg-warning" aria-labelledby="rolesDropdown">
+                        <ul class="dropdown-menu bg-info" aria-labelledby="rolesDropdown">
                             <li><a class="dropdown-item" href="<?php echo get_url('Admin/create_role.php'); ?>">Create</a></li>
                             <li><a class="dropdown-item" href="<?php echo get_url('Admin/list_roles.php'); ?>">List</a></li>
                             <li><a class="dropdown-item" href="<?php echo get_url('Admin/assign_roles.php'); ?>">Assign</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="rolesDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Products
+                        </a>
+                        <ul class="dropdown-menu bg-info" aria-labelledby="rolesDropdown">
+                            <li><a class="dropdown-item" href="<?php echo get_url('Admin/add_items.php'); ?>">Add Products</a></li>
+                            <li><a class="dropdown-item" href="<?php echo get_url('Admin/edit_item.php'); ?>">Edit Products</a></li>
+                            <li><a class="dropdown-item" href="<?php echo get_url('Admin/list_items.php'); ?>">List Products</a></li>
                         </ul>
                     </li>
                 <?php endif; ?>
